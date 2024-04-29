e74ffff1159a2ae85f1cb693c6d86cc8c9390204
Matias Combes <m.combes@alumno.um.edu.ar>
2024-04-29 20:24:20 -0300

    OOP

---
+++ oop.py
@@ -0,0 +1,21 @@
+
+
+
+class Profesor:
+    def __init__(self, el_nombre):
+        self.nommbre = el_nombre 
+
+
+class Alumno:
+    def __init__(self, nombre_alumno):
+        self.nombre = nombre_alumno
+
+alumno_mati = Alumno("Matias")
+alumno_juancito = Alumno("Juan")
+
+profe_elio = Profesor("Elio")
+profe_gabi = Profesor("Gabriel")
+
+
+
+import ipdb; ipdb.set_trace()
